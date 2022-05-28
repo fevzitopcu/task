342a4ceebf8cf409727711ca6953dc9d0921f37a
fevzitopcu <fevzitopcu@gmail.com>
2022-05-28 13:41:30 +0300

    updated

--- task-2a/app.py
+++ task-2a/app.py
@@ -4,8 +4,7 @@ import os
 
 with open('data.json') as json_file:
     data = json.load(json_file)
-    
-    data["built_at"]=os.environ.get('IMAGE_TS')
+    data['built_at'] = os.environ.get('IMAGE_TS')
 
 app = Flask(__name__)
 
@@ -15,4 +14,4 @@ def hello():
     return (data)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run(debug=True, host="0.0.0.0", port=80)
--- task-2b/app.py
+++ task-2b/app.py
@@ -3,15 +3,19 @@ import json
 import os
 
 with open('data.json') as json_file:
-    data = json.load(json_file) 
-    data["deployed_at"]=os.environ.get('DEPLOY_TS')
+    data = json.load(json_file)
+    data["built_at"] = os.environ.get('IMAGE_TS')
+    data["deployed_at"] = os.environ.get('DEPLOY_TS')
+
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
-    
+
     return (data)
 
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run(debug=True, host="0.0.0.0", port=80)
